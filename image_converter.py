4d952097eece6e608aacff31f7978c86271ee246
keval112 <147445346+keval112@users.noreply.github.com>
2024-02-23 22:32:47 +0530

    Update image_converter.py

--- image_converter.py
+++ image_converter.py
@@ -4,7 +4,7 @@ from PIL import Image
 def convert_image(image_path,new_format):
     with Image.open(image_path) as img:
         new_name = image_path.name.split('.')[0]+'.'+ new_format
-        final_path =   new_name
+        final_path =  '/storage/emulated/0/Download' + new_name
         img.save(final_path)
         st.success('image saved at ' + final_path)
 
