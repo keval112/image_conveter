52f6ef5a73b20932a13ecbf3910c60dc10d7f25e
keval112 <147445346+keval112@users.noreply.github.com>
2024-02-23 22:19:04 +0530

    Add files via upload

---
+++ image_converter.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from PIL import Image
+
+def convert_image(image_path,new_format):
+    with Image.open(image_path) as img:
+        new_name = image_path.name.split('.')[0]+'.'+ new_format
+        final_path = 'E:/docs/' + new_name
+        img.save(final_path)
+        st.success('image saved at ' + final_path)
+
+
+
+st.title('Image converter')
+image_path = st.file_uploader('enter your image',type=['png','jpeg','jpg'])
+new_format = st.selectbox('select output format',['png','jpeg','jpg'])
+
+if st.button('convert'):
+    if image_path is not None:
+        convert_image(image_path,new_format)
+    else:
+        st.error("plese,upload the image")
+
